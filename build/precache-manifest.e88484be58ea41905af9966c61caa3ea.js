12a78934a255b49cbb69f8159580ecb91c75bad5
Hieu <nmhieuit@gmail.com>
2022-03-15 14:16:05 +0700

    first comit

---
+++ build/precache-manifest.e88484be58ea41905af9966c61caa3ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9416cb73f96a2b7bd28f721d2b64a09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "133c47d99bcf40e8041c",
+    "url": "/static/css/2.536f1d1e.chunk.css"
+  },
+  {
+    "revision": "b4402f1a6c97a1f0d13a",
+    "url": "/static/css/main.a022480c.chunk.css"
+  },
+  {
+    "revision": "133c47d99bcf40e8041c",
+    "url": "/static/js/2.f93d15d3.chunk.js"
+  },
+  {
+    "revision": "b4402f1a6c97a1f0d13a",
+    "url": "/static/js/main.bafe7baa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
